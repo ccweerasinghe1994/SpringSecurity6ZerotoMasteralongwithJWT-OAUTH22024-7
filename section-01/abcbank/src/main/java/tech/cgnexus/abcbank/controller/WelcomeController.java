8cb6736466832cbae58504d6714bcfbce93258dc
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-12-06 18:22:21 +0530

    Add Spring Security dependencies and enhance WelcomeController logging

--- section-01/abcbank/src/main/java/tech/cgnexus/abcbank/controller/WelcomeController.java
+++ section-01/abcbank/src/main/java/tech/cgnexus/abcbank/controller/WelcomeController.java
@@ -1,13 +1,19 @@
 package tech.cgnexus.abcbank.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    private static final Logger log = LogManager.getLogger(WelcomeController.class);
+
     @GetMapping("/welcome")
     public String welcome() {
-        return "Welcome to ABC Bank! which has no security";
+        String apiURL = "http://localhost:8080";
+        log.info("Welcome API is called. API URL: {}/welcome", apiURL);
+        return "Welcome to ABC Bank! Your security is our priority.";
     }
 }
