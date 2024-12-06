5dc2fb5b85111aa7859231acde4c3210d1e5dbb6
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-12-06 17:08:12 +0530

    initial commit

---
+++ section-01/abcbank/src/main/java/tech/cgnexus/abcbank/AbcbankApplication.java
@@ -0,0 +1,14 @@
+package tech.cgnexus.abcbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcbankApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World! updating");
+		SpringApplication.run(AbcbankApplication.class, args);
+	}
+
+}
